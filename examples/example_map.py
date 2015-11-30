bb3b15cb09f112575927924cea82b128a4598e2c
Clemens Brunner <clemens.brunner@gmail.com>
2015-11-30 15:10:31 +0100

    Added examples (WIP)

---
+++ examples/example_map.py
@@ -0,0 +1,20 @@
+from scipy.io import loadmat
+import matplotlib.pyplot as plt
+import numpy as np
+import erds
+
+
+data = loadmat("test.mat")
+X = data["X"]
+y = data["y"].squeeze()
+
+maps = erds.Erds()
+maps.fit(X[y == 0, :, :])
+
+# plt.plot(maps.erds_[:, 0, 10])
+maps.plot()
+# plt.show()
+# test = maps.erds_[:, 0, 10]
+# f = np.fft.fftfreq(512, 1/512)
+# plt.plot(f[0:256], test[0:256])
+# plt.show()
